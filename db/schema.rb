f57827f9b5dfba709fbe6eeb84c09a51fe150961
vincentbizet <bizetvincent78@gmail.com>
2020-05-23 16:52:51 +0200

    ciao photo and theme

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_135822) do
+ActiveRecord::Schema.define(version: 2020_05_23_144945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cabins", force: :cascade do |t|
     t.string "localisation"
-    t.string "theme"
     t.string "title"
     t.string "description"
-    t.string "photo"
     t.integer "price_per_night"
     t.integer "beds"
     t.bigint "user_id", null: false
