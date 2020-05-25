629823f4b3b6925d12f56ff6665e2a0f2214866b
Mokhtar Boussefsaf <moktab10@gmail.com>
2020-05-25 19:28:01 +0200

    AddMigrationBookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_23_144945) do
+ActiveRecord::Schema.define(version: 2020_05_25_172610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.integer "number_of_guests"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "cabin_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cabin_id"], name: "index_bookings_on_cabin_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "cabins", force: :cascade do |t|
     t.string "localisation"
@@ -41,5 +54,7 @@ ActiveRecord::Schema.define(version: 2020_05_23_144945) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "cabins"
+  add_foreign_key "bookings", "users"
   add_foreign_key "cabins", "users"
 end
