d30f8376a63bde0eeb0808a5de35901aa6baa784
moktab10 <58467735+moktab10@users.noreply.github.com>
2020-05-26 15:39:05 +0200

    Update cabin.rb

--- app/models/cabin.rb
+++ app/models/cabin.rb
@@ -2,8 +2,8 @@ class Cabin < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
-  validates :localisation, presence: true, uniqueness: true
-  validates :title, presence: true, uniqueness: true
+  validates :localisation, presence: true
+  validates :title, presence: true
   validates :description, presence: true
   validates :price_per_night, presence: true
   validates :beds, presence: true
