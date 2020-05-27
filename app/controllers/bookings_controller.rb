326b5e02cc2aa4f393a0b3e1e6488db9abd3a11c
vincentbizet <bizetvincent78@gmail.com>
2020-05-27 21:56:02 +0200

    first part booking controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,18 @@
 class BookingsController < ApplicationController
+  def index
+    @booking = Booking.all
+  end
+  def new
+    @booking = Booking.new
+  end
+    def show
+    @booking = Booking.find(params[:id])
+  end
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to booking_path
+  end
+  def create
+  end
 end
