7381932e194e8c9cd74fd0f36d457128ea4837f6
vincentbizet <bizetvincent78@gmail.com>
2020-05-30 12:43:38 +0200

    little modif

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#cabins = Cabin.create([{ title: 'La cabine de Magalie', description: 'Le paradis sur terre', localisation: 'Marseille', price_per_night: 99, beds: 6 }, { title: 'La cabine de Mokhtar', description: 'Un coin au calme', localisation: 'Lille', price_per_night: 79, beds: 4 }, { title: 'La cabine de Vincent', description: 'Le spot idéal', localisation: 'Arcachon', price_per_night: 59, beds: 2 }])
